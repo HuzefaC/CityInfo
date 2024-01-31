5dfe49ed00e03a99ac408367fdb4910ef485ba74
huzefa <huzefachabukswar@gmail.com>
2024-01-31 13:59:15 +0530

    ENH: Cover returning Files from ASP.NET API

---
+++ CityInfo.API/Controllers/FilesController.cs
@@ -0,0 +1,34 @@
+﻿using System.Net.Mime;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.StaticFiles;
+
+namespace CityInfo.API.Controllers;
+
+[Route("api/files")]
+public class FilesController : Controller
+{
+    private readonly FileExtensionContentTypeProvider _fileExtensionContentTypeProvider;
+
+    public FilesController(FileExtensionContentTypeProvider fileExtensionContentTypeProvider)
+    {
+        _fileExtensionContentTypeProvider = fileExtensionContentTypeProvider;
+    }
+
+    [HttpGet("{fileId}")]
+    public ActionResult GetFile(string fileId)
+    {
+        var filePath = "image-7.jpg";
+        if (!System.IO.File.Exists(filePath))
+        {
+            return NotFound();
+        }
+
+        if (!_fileExtensionContentTypeProvider.TryGetContentType(filePath, out var contentType))
+        {
+            contentType = "application/octet-stream";
+        }
+
+        var fileStream = System.IO.File.ReadAllBytes(filePath);
+        return File(fileStream, contentType, Path.GetFileName(filePath));
+    }
+}
